6ef89653e10811d46ddefb48de3e7d781475aabb
Daniel-098 <josedaniel3938@gmail.com>
2024-08-08 19:26:24 -0600

    Add files via upload

---
+++ Nivel1/preciosmenu.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with JohnDoe landing page.">
+    <meta name="author" content="Devcrud">
+    <title>Smart Park/Precios</title>
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+	<link rel="stylesheet" href="assets/css/johndoe.css">
+    <link rel="stylesheet" href="">
+    <link rel="icon" href="images/preview (1).webp" type="image/x-icon">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    <a href="components.html" class="btn btn-primary btn-component" data-spy="affix" data-offset-top="600"><i class="ti-shift-left-alt"></i> Components</a>
+        <div class="container" style="backgroud color:black">
+            <ul class="social-icons pt-3" c>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="ti-facebook" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="ti-twitter" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="ti-google" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="ti-instagram" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" href="#"><i class="ti-github" aria-hidden="true"></i></a></li>
+            </ul>  
+            <div class="header-content">
+                <h4 class="header-subtitle" >HOLA BIENVENIDOS A</h4>
+                <h1 class="header-title">SMART PARK</h1>
+                <h6 class="header-mono" >ES UN GUSTO RESGUARDARLOS</h6>
+            </div>
+        </div>
+    </header>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white" data-spy="affix" data-offset-top="510">
+        <div class="container">
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse mt-sm-20 navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="menuniveles.php" class="nav-link">NIVELES</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav brand">
+                    <img src="../images/preview (1).webp" alt="" class="brand-img">
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="preciosmenu.php" class="nav-link">PRECIOS</a>
+                    </li>
+                    <li class="nav-item last-item">
+                        <a href="#contact" class="nav-link">CONTACTANOSS</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section class="section bg-custom-gray" id="price">
+        <div class="container">
+            <h1 class="mb-5"><span class="text-danger">Precios</span> de alquileres</h1>
+            <div class="row align-items-center">
+                <div class="col-md-6 col-lg-3">
+                    <div class="price-card text-center mb-4">
+                        <h3 class="price-card-title">Informal</h3>
+                        <div class="price-card-cost">
+                            <sup class="ti-money"></sup>
+                            <span class="num">1</span>
+                            <span class="date">Hrs
+                            </span>
+                        </div>
+                        <ul class="list">
+                            <li class="list-item"><span class="text-muted">Sin protección</span></li>
+                            <li class="list-item"><span class="text-muted">Sin Guias</span></li>
+                            <li class="list-item"><span class="text-muted">Sin chofer</span></li>
+                            <li class="list-item"><span class="text-muted">Sin cargador(electrico)</span></li>
+                        </ul>
+                        <a class="btn btn-primary btn-rounded w-lg" href="menunivelesl.php">Adquirir</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="price-card text-center mb-4">
+                        <h3 class="price-card-title">Basico</h3>
+                        <div class="price-card-cost">
+                            <sup class="ti-money"></sup>
+                            <span class="num">5</span>
+                            <span class="date">Hrs</span>
+                        </div>
+                        <ul class="list">
+                        <li class="list-item"><span class="text-muted">Con protección</span></li>
+                            <li class="list-item"><span class="text-muted">Con Guias</span></li>
+                            <li class="list-item"><span class="text-muted">Sin chofer</span></li>
+                            <li class="list-item"><span class="text-muted">Sin cargador(electrico)</span></li>
+                        </ul>
+                        <a class="btn btn-primary btn-rounded w-lg" href="menunivelesl.php">Adquirir</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="price-card text-center price-card-requried mb-4">
+                        <h3 class="price-card-title">Exclusivo</h3>
+                        <div class="price-card-cost">
+                            <sup class="ti-money"></sup>
+                            <span class="num">10</span>
+                            <span class="date">Hrz</span>
+                        </div>
+                        <ul class="list">
+                        <li class="list-item"><span class="text-muted">Con protección mejorada</span></li>
+                            <li class="list-item"><span class="text-muted">Con Guias</span></li>
+                            <li class="list-item"><span class="text-muted">Sin chofer</span></li>
+                            <li class="list-item"><span class="text-muted">Con cargador(electrico)</span></li>
+                        </ul>
+                        <a class="btn btn-primary btn-rounded w-lg" href="menunivelesl.php">Adquirir</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="price-card text-center mb-4">
+                        <h3 class="price-card-title">Ejecutivo</h3>
+                        <div class="price-card-cost">
+                            <sup class="ti-money"></sup>
+                            <span class="num">45</span>
+                            <span class="date">dia</span>
+                        </div>
+                        <ul class="list">
+                        <li class="list-item"><span class="text-muted">Con protección mejorada</span></li>
+                            <li class="list-item"><span class="text-muted">Con Guias</span></li>
+                            <li class="list-item"><span class="text-muted">Con chofer</span></li>
+                            <li class="list-item"><span class="text-muted">Con cargador(electrico)</span></li>
+                        </ul>
+                        <a class="btn btn-primary btn-rounded w-lg" href="menunivelesl.php">Adquirir</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section bg-dark py-5">
+        <div class="container text-center">
+            <h2 class="text-light mb-5 font-weight-normal">Registrate para poder adquirir nustros servicios</h2>
+            <a class="btn bg-primary w-lg" href="registro.php">REGISTRATE</a>
+        </div>
+    </section>
+
+    <footer class="footer py-3">
+        <div class="container">
+            <p class="small mb-0 text-light">
+                &copy; <script>document.write(new Date().getFullYear())</script> Createdo por<i class="ti-heart text-danger"></i> <a href="http://devcrud.com" target="_blank"><span class="text-danger" title="Bootstrap 4 Themes and Dashboards">Liceo Canadiense</span></a> 
+            </p>
+        </div>
+    </footer>
+
+	<!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap 3 affix -->
+    <script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- Isotope  -->
+    <script src="assets/vendors/isotope/isotope.pkgd.js"></script>
+    
+    <!-- Google mpas -->
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap"></script>
+
+    <!-- JohnDoe js -->
+    <script src="assets/js/johndoe.js"></script>
+
+</body>
+</html>
